a065eca4d62a970eb4445f71e9a5ffdd17e75c5f
Leo Oviedo <leoxpro050@gmail.com>
2024-02-14 01:56:48 -0300

    Agregando la seccion proyectos

--- main.js
+++ main.js
@@ -6,20 +6,28 @@ btnMenu.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const enlaceContacto = document.querySelector('a[href= "#footer"]');
-    const enlaceSobreMi = document.querySelector('a[href= "#body"]')
+    const enlaceSobreMi = document.querySelector('a[href= "#sobreMi"]');
+    const enlaceProyectos = document.querySelector('a[href= "#proyectos"]');
 
     enlaceSobreMi.addEventListener('click', (event) => {
         event.preventDefault();
-        const sobreMi = document.querySelector('.container-body')
-        sobreMi.scrollIntoView( {behavior: 'smooth'} )
+        const sobreMi = document.querySelector('.contenido-sobreMi');
 
+        sobreMi.scrollIntoView({ behavior: 'smooth' })
     });
-    
+
     enlaceContacto.addEventListener('click', (event) => {
         event.preventDefault();
         const contacto = document.querySelector('.footer-list-description');
 
-        contacto.scrollIntoView( {behavior: 'smooth'} )
+        contacto.scrollIntoView({ behavior: 'smooth' })
     });
-});
 
+    enlaceProyectos.addEventListener('click', (event) => {
+        event.preventDefault();
+        const proyectos = document.querySelector('.contenido-proyectos');
+
+        proyectos.scrollIntoView( {behavior: 'smooth'} )
+    })
+
+});
